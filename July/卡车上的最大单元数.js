59f88297e5f6c0666c6dad56965adf9c2358e4c9
thomas <thomas.sunflower1@gmail.com>
2023-07-25 21:57:50 +0800

    修改卡车...变量

--- July/卡车上的最大单元数.js
+++ July/卡车上的最大单元数.js
@@ -4,27 +4,27 @@
  * @return {number}
  */
 
-function fun(array, max) {
+function fun(boxTypes, truckSize) {
   let temp = [];
   let sum = 0;
   //   使用冒泡排序,从小到大排序
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - i - 1; j++) {
-      if (array[j][1] > array[j + 1][1]) {
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+  for (let i = 0; i < boxTypes.length - 1; i++) {
+    for (let j = 0; j < boxTypes.length - i - 1; j++) {
+      if (boxTypes[j][1] > boxTypes[j + 1][1]) {
+        temp = boxTypes[j];
+        boxTypes[j] = boxTypes[j + 1];
+        boxTypes[j + 1] = temp;
       }
     }
   }
   //   再一次利用for循环进行求和
-  for (let i = array.length - 1; i >= 0; i--) {
-    if (max >= array[i][0]) {
-      sum += array[i][0] * array[i][1];
-      max -= array[i][0];
+  for (let i = boxTypes.length - 1; i >= 0; i--) {
+    if (truckSize >= boxTypes[i][0]) {
+      sum += boxTypes[i][0] * boxTypes[i][1];
+      truckSize -= boxTypes[i][0];
     } else {
-      sum += max * array[i][1];
-      max = 0;
+      sum += truckSize * boxTypes[i][1];
+      truckSize = 0;
     }
   }
   return sum;
